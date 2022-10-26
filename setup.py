4668c010e5a5638535835a03cbd1712a74b50db8
mirencagigal <miren.cagigal@alumni.mondragon.edu>
2022-10-26 23:28:34 +0200

    Modificamos la version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'libreria_correlaciones',         # How you named your package folder (MyLib)
   packages = ['libreria_correlaciones'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Esta librería sirve para la deteccion de correlaciones de cara a limpiar los datos para un modelo de clasificacion binaria.',   # Give a short description about your library
   author = 'Paula',                   # Type in your name
