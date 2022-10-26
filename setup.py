eef6391db095d4ddce2e2bd8f0becfa1d45d6a45
mirencagigal <miren.cagigal@alumni.mondragon.edu>
2022-10-26 23:00:00 +0200

    Actualizamos el setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Paula',                   # Type in your name
   author_email = 'paula.hermosilla@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/PaulaHermosilla/libreria_correlaciones',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['CORRELACIONES', 'CLASIFICACION_BINARIA', 'HISTOGRAMAS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
