bd28d0957645a9839dabebaa3e035c1e5fa476eb
mirencagigal <miren.cagigal@alumni.mondragon.edu>
2022-10-31 20:30:18 +0100

    Mejoras en el .py

--- libreria_correlaciones/libreria_correlaciones.py
+++ libreria_correlaciones/libreria_correlaciones.py
@@ -13,19 +13,22 @@ class deteccion_correlaciones:
     def eliminacion_variables_correlacionadas(self):
         corr_matrix = self.variables_corr_para_modelar.corr().abs()
         upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))  # cogemos el triángulo superior para que solamente nos elimine 1 variable.
-        to_drop = [column for column in upper.columns if any(upper[column] > 0.3)]
+        to_drop = [column for column in upper.columns if any(upper[column] > 0.8)]
         nuevo_df = self.df.drop(columns = to_drop)
         return nuevo_df
-
+     
     def variables_mas_importantes(self):
         corr_matrix2 = self.numericas.corr().abs()
         upper2 = corr_matrix2.where(np.triu(np.ones(corr_matrix2.shape), k=1).astype(np.bool))
+        variable_objetivo = [(i) for i in self.numericas.columns if self.numericas[i].isin([-1,1]).all() | self.numericas[i].isin([0,1]).all()][0]
+        
         variables_mas_importantes = []
+        corr_clase = upper2[variable_objetivo]
+        
         for i in range(0,upper2.shape[0]): 
-            if (upper2.iloc[i,(upper2.shape[1])-1] > 0.1):     
-                variables_mas_importantes.append(upper2.index[i])
+                if (corr_clase[i] > 0.3):     
+                        variables_mas_importantes.append(corr_clase.index[i])
         return variables_mas_importantes
-     
 
     # y es el valor que hay que meter dependiendo de las variables que se tenga 
     def histogramas(self,datos): #datos tiene que ser el resultado de la función variables_mas_importantes 
