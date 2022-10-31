06f6ca2608c2d46067a7c8e4cffe2e298318d86e
mirencagigal <miren.cagigal@alumni.mondragon.edu>
2022-10-31 21:04:01 +0100

    Subida version definitiva

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'libreria_correlaciones',         # How you named your package folder (MyLib)
   packages = ['libreria_correlaciones'],   # Chose the same as "name"
-  version = '0.6',      # Start with a small number and increase it with every change you make
+  version = '0.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Esta librería sirve para la deteccion de correlaciones de cara a limpiar los datos para un modelo de clasificacion binaria.',   # Give a short description about your library
   author = 'Paula',                   # Type in your name
   author_email = 'paula.hermosilla@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/PaulaHermosilla/libreria_correlaciones',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_06.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_07.tar.gz',    # I explain this later on
   keywords = ['CORRELACIONES', 'CLASIFICACION_BINARIA', 'HISTOGRAMAS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
