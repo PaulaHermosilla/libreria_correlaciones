e712b85fa1807dd5312db50fc65a5c4d7cb09205
mirencagigal <miren.cagigal@alumni.mondragon.edu>
2022-11-03 16:15:17 +0100

    Ultima modificacion en el readme

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 from pathlib import Path
 directory = Path(__file__).parent
 long_description = (directory / "README.md").read_text()
 setup(
   name = 'libreria_correlaciones',         # How you named your package folder (MyLib)
   packages = ['libreria_correlaciones'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
+  version = '0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Esta librería sirve para la deteccion de correlaciones de cara a limpiar los datos para un modelo de clasificacion binaria.',   # Give a short description about your library
   author = 'Paula',                   # Type in your name
   author_email = 'paula.hermosilla@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/PaulaHermosilla/libreria_correlaciones',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_08.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_09.tar.gz',    # I explain this later on
   keywords = ['CORRELACIONES', 'CLASIFICACION_BINARIA', 'HISTOGRAMAS'],   # Keywords that define your package best
   long_description=long_description,
   long_description_content_type="text/markdown",
